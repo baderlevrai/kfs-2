ea9f2badd9db56cc4ace7c930e1dc1187b020f22
iouajjou <iouajjou@outlook.com>
2026-03-11 23:54:02 +0100

    feat: backspace delete last charac

--- src/kernel.h
+++ src/kernel.h
@@ -56,6 +56,8 @@ int		strcmp(const char *s1, const char *s2);
 void	kbd_handler(void);
 void	kbd_init(void);
 
+void handle_backspace();
+
 int printk(const char *format, ...);
 
 inline void outb(uint16_t port, uint8_t val)
--- src/terminal.c
+++ src/terminal.c
@@ -102,12 +102,12 @@ void terminal_write(const char* data, size_t size)
 }
 
 void handle_backspace() {
-    if (terminal_row == 0) return;
+	if (terminal_column <= 11) return; //Juste avant le prompt
 
-	terminal_row--;
+	terminal_column--;
 
-	terminal_putchar(' ');
-	
+	terminal_putentryat(' ', terminal_color, terminal_column, terminal_row);
+
 	update_cursor(terminal_column, terminal_row);
 }
 
